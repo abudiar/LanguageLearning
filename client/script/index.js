a3064e473fd531f3335810b228eaa7ffd45391ca
RuliSlim <ruli0to8@gmail.com>
2020-04-03 02:47:21 +0700

    tinggal tarik file dari server

--- client/script/index.js
+++ client/script/index.js
@@ -1,50 +1,40 @@
-// // const pcm = require('pcm');
 $( document ).ready(function() {
-  $.ajax({
-    url: "http://localhost:3000/voices",
-    method: "post",
-  })
-  .then((result) => {
-    console.log(result)
-    // var blob = new Blob(decodedData, {type: "correct-mimetype/here"});
-    // var url = URL.createObjectURL(blob);
-    // // audio.src = url
-    console.log('masuk ga sih')
-    // var sine = [result.data];
-    // for (var i=0; i<10000; i++) {
-    //   sine[i] = 128+Math.round(127*Math.sin(i/5));
-    // }
-            // audio.pause();
-        // audio.src = URL.createObjectURL(response.data);
-        // audio.play();
+  const dataLang = [
+    'ar-AR_OmarVoice','de-DE_BirgitVoice','de-DE_BirgitV3Voice','de-DE_DieterVoice','de-DE_DieterV3Voice','de-DE_ErikaV3Voice','en-GB_KateVoice','en-GB_KateV3Voice','en-US_AllisonVoice','en-US_AllisonV3Voice','en-US_EmilyV3Voice','en-US_HenryV3Voice','en-US_KevinV3Voice','en-US_LisaVoice','en-US_LisaV3Voice','en-US_MichaelVoice','en-US_MichaelV3Voice','en-US_OliviaV3Voice','es-ES_EnriqueVoice','es-ES_EnriqueV3Voice','es-ES_LauraVoice','es-ES_LauraV3Voice','es-LA_SofiaVoice','es-LA_SofiaV3Voice','es-US_SofiaVoice','es-US_SofiaV3Voice','fr-FR_ReneeVoice','fr-FR_ReneeV3Voice','it-IT_FrancescaVoice','it-IT_FrancescaV3Voice','ja-JP_EmiVoice','ja-JP_EmiV3Voice','nl-NL_EmmaVoice','nl-NL_LiamVoice','pt-BR_IsabelaVoice','pt-BR_IsabelaV3Voice','zh-CN_LiNaVoice','zh-CN_WangWeiVoice','zh-CN_ZhangJingVoice'
+  ]
 
-    
-    // new pcm({channels: 1, rate: 8000, depth: 8}).toWav(sine).play();
-    // var dataURI = new pcm({channels: 1, rate: 8000, depth: 8}).toWav(sine).encode();
-    $('#audio').append(`<source src="${URL.createObjectURL(result)}" type="audio/wav">`)
+  let dom;
+  dataLang.forEach(el => {
+    dom += `<option value="${el}">${el}</option><br>`;
   })
-  .fail(fail => {
-    console.log(fail)
+
+  $('#voice').append(`
+    <select id="lang" name="lang">
+      ${dom}
+    </select>
+  `)
+
+  $('#voice').submit(function(e) {
+    e.preventDefault();
+    const data = {
+      text: $('#text').val(),
+      voice: $('#lang').val()
+    }
+  
+    console.log(data)
+  
+    $.ajax({
+      url: `http://localhost:3000/voices?voice=${data.voice}&text=${data.text}`,
+      method: 'POST',
+      data: data
+    })
+    .then(result => {
+      console.log(result);
+      console.log('masuk ga sih');
+      $('#audio').html(`<source src="nama3.wav" type="audio/wav">`);
+    })
+    .fail(fail => {
+      console.log(fail);
+    })
   })
 })
-
-// var audio;
-// fetch('http://localhost:3000/voices')
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(token) {
-//     audio = WatsonSpeech.TextToSpeech.synthesize(Object.assign(token, {
-//       text: 'Hello from IBM Watson',
-//       autoPlay: false,
-//       accept: 'audio/wav'
-//     }));
-
-//     audio.onerror = function(err) {
-//       console.log('audio error: ', err);
-//     };
-//   });
-
-// document.querySelector('#button').onclick = function() {
-//   audio.play();
-// };
--- server/controllers/voice.js
+++ server/controllers/voice.js
@@ -25,52 +25,77 @@ const getVoiceParams = {
 
 class VoiceController {
   static getVoice(req, res, next) {
-    // var transcript = textToSpeech.synthesize(req.query);
-    // transcript.on('error', next);
-    // transcript.pipe(res)
-    axios({
-      method: 'post',
-      url: 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/f8a5f28e-e529-41d9-bd32-7ff3509c5043/v1/synthesize?voice=en-US_LisaVoice',
-      headers: {
-        accept: 'audio/wav',
-        'content-type': 'application/json',
-        authorization: 'Basic YXBpa2V5Oi1rY2tyZGw4VDdURmlIRHpjWHhlN3p4MUo1aVMzMVowOGNFUHhfb0tnYUM1'
-      },
-      data: {
-        text: 'Yo, bisa ga?',
-      },
-      responseType: 'blob'
-    })
-      .then(audio => {
-        console.log(typeof audio.data)
-        // res.status(200).json({data: audio.data})
-        // audio.pause();
-        // audio.src = URL.createObjectURL(response.data);
-        // audio.play();
-        res.send(audio.data)
-        // res.send(`<audio controls>
-        //   <source src="${audio.data}" type="audio/wav"></audio>
-        // `)
-      })
-      .catch(err => {
-        res.status(500).send(err.message)
-        console.log(err, 'error')
-      })
-
-    // const synthesizeParams = {
-    //   text: 'Hello world',
-    //   accept: 'audio/wav',
-    //   voice: 'en-US_AllisonVoice',
-    // }
-    
-    // textToSpeech.synthesize(synthesizeParams)
+    console.log(req.query, 'dari client')
+    console.log(req.data, 'dari client')
+    console.log(req.body, 'dari client')
+    const {voice, text} = req.query
+    // axios({
+    //   method: 'post',
+    //   url: 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/f8a5f28e-e529-41d9-bd32-7ff3509c5043/v1/synthesize?voice=' + lang,
+    //   headers: {
+    //     accept: 'audio/wav',
+    //     'content-type': 'application/json',
+    //     authorization: 'Basic YXBpa2V5Oi1rY2tyZGw4VDdURmlIRHpjWHhlN3p4MUo1aVMzMVowOGNFUHhfb0tnYUM1'
+    //   },
+    //   data: {
+    //     text
+    //   }
+    //   // responseType: 'blob'
+    // })
     //   .then(audio => {
-    //     audio.result.pipe(fs.createWriteStream('../client/kobeda.wav'));
-    //     res.status(200).send('udah')
+    //     // console.log(typeof audio.data)
+    //     // res.status(200).json({data: audio.data})
+    //     // audio.pause();
+    //     // audio.src = URL.createObjectURL(response.data);
+    //     // audio.play();
+    //     res.send(audio.data)
+    //     // res.send(`<audio controls>
+    //     //   <source src="${audio.data}" type="audio/wav"></audio>
+    //     // `)
     //   })
     //   .catch(err => {
-    //     console.log('error:', err);
-    //   });
+    //     res.status(500).send(err.message)
+    //     // console.log(err, 'error')
+    //   })
+
+      // const synthesizeParams = { text: 'This is test from server yo', accept: 'audio/wav', voice: 'en-US_AllisonVoice', };
+
+      // textToSpeech.synthesize(synthesizeParams)
+      //     .then(audio => {
+      //         console.log("Audio Received : ");
+      //         var blob=new Blob(audio, {type : 'audio/wav'});
+      //         res.send(blob);
+      // })
+
+      // const synthesizeParams = {
+      //     text: 'Hello world',
+      //     accept: 'audio/wav',
+      //     voice: 'en-US_AllisonVoice',
+      //   }
+        
+      //   textToSpeech.synthesize(synthesizeParams)
+      //     .then(audio => {
+      //       audio.result.pipe(fs.createWriteStream('../client/kobeda.wav'));
+      //       res.status(200).send('kobeda')
+      //     })
+      //     .catch(err => {
+      //       console.log('error:', err);
+      //     });
+
+    const synthesizeParams = {
+      text,
+      voice,
+      accept: 'audio/wav',
+    }
+    
+    textToSpeech.synthesize(synthesizeParams)
+      .then(audio => {
+        audio.result.pipe(fs.createWriteStream('../client/nama3.wav'));
+        res.status(200).send('nama')
+      })
+      .catch(err => {
+        console.log('error:', err);
+      });
 
   }
 }
