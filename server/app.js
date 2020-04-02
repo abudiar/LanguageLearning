4875b28fac6fe67b02a5257e786d3463b8a919c2
RuliSlim <ruli0to8@gmail.com>
2020-04-02 15:50:14 +0700

    create initial setup server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app     = express();
+const cors    = require('cors');
+const PORT    = process.env.PORT || 3000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routers
+app.use('/voices', require('./routes/voice'))
+
+// start
+app.listen(PORT, () => {console.log(`Server start on port: ${PORT}`);});
